2731dfba20acd2a4ab3308803e28f6dad9199206
Antoine Libessart <antoine.libessart@epsi.fr>
2019-12-16 13:41:26 +0100

    TestIntegration-2

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 37;
+        int b = 54;
+        int c = 84;
+        int d = b + c;
+        System.out.println(c + " Est le plus grand chiffre");
+        System.out.println(d);
+    }
+    }
+
